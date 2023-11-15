bd6a0620e8af7cc9ccca8d5917425fc91914b662
youdipugo <kzryanarinzolojt@gmail.com>
2023-11-15 09:16:08 +0800

    HTML

---
+++ resources/views/signin.blade.php
@@ -0,0 +1,44 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> Sign Up Now</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+
+<div class="wrapper">
+<div class="wrapper-left">
+    <div class="logo">
+        <img src="img/vvv.jpg.png" id="qwert">
+        <img src="img/RRR.png" id="asa">
+    </div>
+    </div>
+<div class="wrapper-right">
+    <div class="forms">
+        
+        <h1> Sign Up Now</h1>
+        <form id="sign-up">
+        <div class="column">
+            <div class="select-box">
+                <select>
+                    <option>designation</option>
+                </select>
+            </div>  
+        <input type="text" class="input-box" placeholder="First name">
+        <input type="text" class="input-box" placeholder="Middle name">
+        <input type="text" class="input-box" placeholder="Last name">
+        <input type="text" class="input-box" placeholder="Username">
+        <input type="Password" class="input-box" placeholder="Password">
+        <input type="button" class="btn" value="Sign Up">
+    </form> 
+
+
+    </div>
+
+</div>
+</div>
+
+</body>
+</html>
